69c9dcfb95cc2d20a883aecc01303d99cd3b8e3b
danielsergio22 <ds.daniel.sergio@gmail.com>
2024-06-19 23:04:18 -0300

    voltando tudo que foi feito ate o primeiro commit

--- FAWS/Expedicao/interface-wms/Classes/GerenciarAcessos.cs
+++ FAWS/Expedicao/interface-wms/Classes/GerenciarAcessos.cs
@@ -6,9 +6,6 @@ namespace Interface_WMS_Expedicao.Classes
     class GerenciarAcessos
     {
 
-        private static string NivelHierarquico; //Melhoria 2
-        private static string NivelPermissao; //Melhoria 1
-
         private static string Usuario;
         private static string ResultLogin;
 
